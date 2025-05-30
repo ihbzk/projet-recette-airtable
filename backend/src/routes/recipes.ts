13f5c7bae4be9916ec5166cd1ffe47f5e2873b02
Ilyes Kabrine <ilyeskabrine0@gmail.com>
2025-05-30 15:20:18 +0200

    add search recipes & high/low calories recipes endpoints

--- backend/src/routes/recipes.ts
+++ backend/src/routes/recipes.ts
@@ -10,5 +10,9 @@ router.get("/api/recipes", RecipesController.getAllRecipes);
 router.post("/api/addRecipes", RecipesController.addRecipe);
 router.post("/api/generate", RecipesController.generateRecipe);
 // router.post("/api/generate", RecipesController.generateRecipe);
+router.get("/api/searchRecipes", RecipesController.searchRecipes);
+router.get("/api/lowCaloriesRecipes", RecipesController.getLowCaloriesRecipes);
+router.get("/api/highCaloriesRecipes", RecipesController.getHighCaloriesRecipes);
+router.get("/api/recipe/:id", RecipesController.getRecipeById);
 
 export default router;
